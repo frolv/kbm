24fe3eeec0b71cb405a9acad4e2b8a3fcd612a2b
Alexei Frolov <alexeifrolov3@gmail.com>
2016-08-19 00:01:10 -0400

    Add more key definitions to keymap

--- src/keymap.h
+++ src/keymap.h
@@ -19,9 +19,72 @@
 #ifndef _KEYMAP_H
 #define _KEYMAP_H
 
-#define KEY_Q 1
-#define KEY_W 2
-#define KEY_E 3
+/* kbm keycodes */
+
+/* alphabetic keys */
+#define KEY_Q 0x01
+#define KEY_W 0x02
+#define KEY_E 0x03
+#define KEY_R 0x04
+#define KEY_T 0x05
+#define KEY_Y 0x06
+#define KEY_U 0x07
+#define KEY_I 0x08
+#define KEY_O 0x09
+#define KEY_P 0x0A
+#define KEY_A 0x0B
+#define KEY_S 0x0C
+#define KEY_D 0x0D
+#define KEY_F 0x0E
+#define KEY_G 0x0F
+#define KEY_H 0x10
+#define KEY_J 0x11
+#define KEY_K 0x12
+#define KEY_L 0x13
+#define KEY_Z 0x14
+#define KEY_X 0x15
+#define KEY_C 0x16
+#define KEY_V 0x17
+#define KEY_B 0x18
+#define KEY_N 0x19
+#define KEY_M 0x1A
+
+/* numeric keys */
+#define KEY_1 0x1B
+#define KEY_2 0x1C
+#define KEY_3 0x1D
+#define KEY_4 0x1E
+#define KEY_5 0x1F
+#define KEY_6 0x20
+#define KEY_7 0x21
+#define KEY_8 0x22
+#define KEY_9 0x23
+#define KEY_0 0x24
+
+/* other keys */
+#define KEY_BTICK	0x25
+#define KEY_MINUS	0x26
+#define KEY_EQUAL	0x27
+#define KEY_LSQR	0x28
+#define KEY_RSQR	0x29
+#define KEY_BSLASH	0x2A
+#define KEY_SEMIC	0x2B
+#define KEY_QUOTE	0x2C
+#define KEY_COMMA	0x2D
+#define KEY_PERIOD	0x2E
+#define KEY_FSLASH	0x2F
+#define KEY_SPACE	0x30
+
+/* modifiers and special keys */
+#define KEY_ESCAPE	0x31
+#define KEY_BSPACE	0x32
+#define KEY_TAB		0x33
+#define KEY_CAPS	0x34
+#define KEY_ENTER	0x35
+#define KEY_SHIFT	0x36	/* don't bother distinguishing	*/
+#define KEY_CTRL	0x37	/* between the left and right	*/
+#define KEY_SUPER	0x38	/* versions of these keys;	*/
+#define KEY_META	0x39	/* treat them identically	*/
 
 /* keystr: return a string representation of key corresponding to keycode */
 char *keystr(unsigned int keycode);
